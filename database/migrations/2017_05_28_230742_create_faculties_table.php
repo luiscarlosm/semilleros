0d4a2b218de02691466569a61150560f6bbfb4d1
Lina Fernanda <ferchalina88@hotmail.com>
2017-05-28 18:20:27 -0500

    Create_migration_faculties_table

---
+++ database/migrations/2017_05_28_230742_create_faculties_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFacultiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faculties', function (Blueprint $table) {
+            $table->increments('id');
+            $table->String('name');
+            $table->integer('sectionals_id')->unsigned();
+            $table->foreign('sectionals_id')->references('id')->on('sectionals');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('faculties');
+    }
+}
