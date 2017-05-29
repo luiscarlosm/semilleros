8179d85b33aa2da60a892ab901f333a32088e695
Lina Fernanda <ferchalina88@hotmail.com>
2017-05-28 23:04:46 -0500

    modifique los campos facultiesController

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', 'SectionalsController@listSectionals');
 
 Route::post('/sectionals/create', 'SectionalsController@create');
 Route::get('/sectionals', 'SectionalsController@listSectionals');
-Route::get('/falculties', 'FacultiesController@listFalcuties');
+Route::get('/faculties', 'FacultiesController@listFalcuties');
 Route::get('/projects', 'ProjectsController@listProjects');
 Route::get('/research_centers', 'ResearchCentersController@listResearchCenters');
 Route::get('/events', 'EventsController@listEvents');
