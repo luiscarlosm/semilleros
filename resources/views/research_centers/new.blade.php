8db0c18cd0f7fe6729d48f2d3c27f9d626cdcb24
unknown <robertodavidrobles@gmail.com>
2017-05-29 15:17:43 -0500

    create new view

---
+++ resources/views/research_centers/new.blade.php
@@ -0,0 +1,31 @@
+@include('layouts.head')
+
+@include('layouts.nav')
+
+<br/>
+
+<section class="row">
+  <div class="col s6">
+    <form action="/sectionals/create" method="post">
+    	<div class="input-field">
+    		{{ csrf_field() }}
+    		<input type="text" name="name" class="form-control" placeholder="Nombre">
+    	</div>
+      <div class="input-field">
+    		{{ csrf_field() }}
+    		<input type="date" name="init_date" class="form-control" placeholder="Fecha de inicio">
+    	</div>
+      <div class="input-field">
+    		{{ csrf_field() }}
+    		<input type="text" name="observation" class="form-control" placeholder="Observaciones">
+    	</div>
+      <div class="input-field">
+    		{{ csrf_field() }}
+    		<input type="number" name="groups_id" class="form-control" placeholder="ID grupo">
+    	</div>
+      <div class="input-field">
+        <button type="submit" name="create" class="btn light-blue darken-4">Crear Seccional</button>
+      </div>
+    </form>
+  </div>
+</section>
