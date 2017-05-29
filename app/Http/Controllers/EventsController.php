16fa510cfbb06161748d9903ccf7aa292f7ffaa8
Lina Fernanda <ferchalina88@hotmail.com>
2017-05-28 22:20:08 -0500

    create model, controller and router and table Events

---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Events;
+use Illuminate\Http\Request;
+
+class EventsController extends Controller
+{
+  public function listEvents() {
+
+    $events = Event::all();
+
+    return view('welcome', [
+      'events' => $events,
+    ]);
+  //
+}
+}
