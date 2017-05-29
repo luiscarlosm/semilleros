a0cae40c61357ed4e8c0e2789f4b333f5e7925af
Juan Roa <jdroa92@gmail.com>
2017-05-28 21:43:48 -0500

    Adding basic nav

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,10 @@
+<nav>
+  <div class="nav-wrapper light-blue lighten-3">
+    <a href="#" class="brand-logo center">Semilleros UCC</a>
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li><a href="/sectionals">Sectionals</a></li>
+      <li><a href="/faculties">Faculties</a></li>
+      <li><a href="/groups">Groups</a></li>
+    </ul>
+  </div>
+</nav>
