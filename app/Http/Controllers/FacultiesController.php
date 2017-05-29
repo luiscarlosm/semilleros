df1ec76f5fe7831926b5ff10bf59035bb1723b9f
Lina Fernanda <ferchalina88@hotmail.com>
2017-05-28 21:36:44 -0500

    route and controller for faculty created

--- app/Http/Controllers/FacultiesController.php
+++ app/Http/Controllers/FacultiesController.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Faculty;
 use Illuminate\Http\Request;
 
 class FacultiesController extends Controller
 {
-    //
+  public function listFalcuties() {
+
+    $falcuties = Faculty::all();
+
+    return view('welcome', [
+      'faculties' => $faculties,
+    ]);
+
+  }
+  //
 }
